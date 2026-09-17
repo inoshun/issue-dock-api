e9500d72f8f41b188bb6c778cb5b53e86864a7a8
Shun Inoue <shun.inoue11@gmail.com>
2026-09-17 19:10:37 +0900

    Add Swagger integration for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('IssueDock')
+    .setDescription('The IssueDock API description')
+    .setVersion('1.0')
+    .addTag('auth', '認証')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
